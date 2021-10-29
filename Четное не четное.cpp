0a82ee18f83b8d0b1176192ca04978826ed011d7
Зубенко Михаил Петрович <70808686+Savka-bot@users.noreply.github.com>
2021-10-29 18:27:44 +0300

    Add files via upload

---
+++ Четное не четное.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int num;
+    cout << "Введите число: ";
+    cin >> num;
+
+    if (num % 2 != 0)
+        cout << "Число " << num << " нечетное" << endl;
+    else
+        cout << "Число " << num << " четное" << endl;
+
+    system("pause");
+
+    return 0;
+}
---
+++ найбольший делитель числа.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+
+
+using namespace std;
+
+
+int main() 
+{
+
+    setlocale(LC_ALL, "Rus");
+
+    int n;
+
+    cin >> n;
+
+    int i = n / 2;
+    while (n % i != 0) i--;
+    if (i != 1) cout << "наиб. делитель " << i << endl;
+    else cout << "число простое \n";
+    system("pause");
+    return 0;
+}
